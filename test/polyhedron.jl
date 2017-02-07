d7238bc826c2b06dcb48fca339d7dc03ca9cf70f
Benoît Legat <benoit.legat@gmail.com>
2017-02-07 11:55:20 +0100

    Fix #5

--- test/polyhedron.jl
+++ test/polyhedron.jl
@@ -4,12 +4,12 @@ include(joinpath(polyhedra_test, "alltests.jl"))
 
 tests = Tuple{String, Function}[]
 push!(tests, ("Hypercube in 2 dimensions", lib->hypercubetest(lib, 2)))
-push!(tests, ("Simplex in 2 dimensions", lib->simplextest(lib, 2)))
+#push!(tests, ("Simplex in 2 dimensions", lib->simplextest(lib, 2))) # linearity
 push!(tests, ("Simplex with the origin in 2 dimensions", lib->simplexorigtest(lib, 2)))
 push!(tests, ("Cross Polytope in 2 dimensions", lib->crosspolytopetest(lib, 2)))
 push!(tests, ("The ex1 example", ex1test))
 push!(tests, ("Infeasible in 2 dimensions", lib->infeasibletest(lib, 2)))
-push!(tests, ("Non full-dimensional", nonfulldimensionaltest))
+#push!(tests, ("Non full-dimensional", nonfulldimensionaltest)) #linearity
 
 @testset "Polyhedra tests in $arith arithmetics" for arith in [:float, :exact]
     runtests(ConvexHullLib(arith), tests)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,10 @@
-using ConvexHull, JuMP, Base.Test
+using ConvexHull, JuMP, Polyhedra, Base.Test
+
+@testset "Throws error in case of linearity" begin
+    simplex = LiftedHRepresentation([0 1 0; 0 0 1; 1 -1 -1], IntSet([3]))
+    @test_throws ErrorException double_description(simplex)
+    linv = LiftedHRepresentation([1 1], IntSet([1]))
+    @test_throws ErrorException double_description(linv)
+end
 
 include("polyhedron.jl")
