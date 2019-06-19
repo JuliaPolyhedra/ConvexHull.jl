c2632345d612821bceb95ffb3f6e22e30090bb24
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-06-19 16:59:13 +0200

    tests passing

--- test/polyhedron.jl
+++ test/polyhedron.jl
@@ -3,8 +3,40 @@ import Pkg
 const POLYHEDRA_TEST = joinpath(dirname(dirname(pathof(Polyhedra))), "test")
 
 include(joinpath(POLYHEDRA_TEST, "utils.jl"))
+include(joinpath(POLYHEDRA_TEST, "config.jl"))
 include(joinpath(POLYHEDRA_TEST, "solvers.jl")) # defines lp_solver
-include(joinpath(POLYHEDRA_TEST, "polyhedra.jl"))
+include(joinpath(POLYHEDRA_TEST, "jump.jl"))
+
+include(joinpath(POLYHEDRA_TEST, "basic.jl"))
+include(joinpath(POLYHEDRA_TEST, "simplex.jl"))
+include(joinpath(POLYHEDRA_TEST, "permutahedron.jl"))
+include(joinpath(POLYHEDRA_TEST, "board.jl"))
+include(joinpath(POLYHEDRA_TEST, "docexample.jl"))
+include(joinpath(POLYHEDRA_TEST, "issue48.jl"))
+include(joinpath(POLYHEDRA_TEST, "empty.jl"))
+include(joinpath(POLYHEDRA_TEST, "sparse.jl"))
+include(joinpath(POLYHEDRA_TEST, "sparserect.jl"))
+include(joinpath(POLYHEDRA_TEST, "support_function.jl"))
+const misctests = Dict("basic" => basictest,
+                       "doc" => doctest,
+                       "simplex" => simplextest,
+                       "permutahedron" => permutahedrontest,
+                       "board" => boardtest,
+                       "issue48" => issue48test,
+                       "empty" => emptytest,
+                       "sparse" => sparsetest,
+                       "sparserect" => sparserecttest,
+                       "support_function" => support_function_test)
+
+@polytestset misc
+
+include(joinpath(POLYHEDRA_TEST, "decompose.jl"))
+
+polyhedratests = Dict("jump" => jumptest,
+                      "misc" => misctest,
+                      "decompose" => decomposetest)
+
+@polytestset polyhedra true
 
 @testset "Polyhedra tests in $arith arithmetics" for arith in [:float, :exact]
     polyhedratest(ConvexHull.Library(arith, lp_solver), [
